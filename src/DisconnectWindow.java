01917291d33ddf3062f45ef05a5cc3e9fdf27c2f
Gr3mlin106 <maciej.matuszewski@me.com>
2016-01-22 00:26:25 +0100

    Update 18

---
+++ src/DisconnectWindow.java
@@ -0,0 +1,46 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class DisconnectWindow extends JFrame {
+
+	private JPanel contentPane;
+	
+	public ObjectOutputStream output = null;
+	public String partnerIP = "";
+
+	public DisconnectWindow(String partnerIP, ObjectOutputStream output) {
+		this.output = output;
+		this.partnerIP = partnerIP;
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setBounds(100, 100, 222, 134);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JButton btnRozcz = new JButton("Rozłącz");
+		contentPane.add(btnRozcz, BorderLayout.CENTER);
+		DisconnectWindow dw = this;
+		btnRozcz.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Main.sendBye(partnerIP, output);
+				dw.dispose();
+				
+			}
+		});
+		
+		this.setVisible(true);
+	}
+
+}
